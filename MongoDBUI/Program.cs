b7cc5d8bb81cad32e4512b40f2ac48768aba1413
yoomster <nperenboom@hotmail.com>
2023-12-02 18:51:02 +0100

    Add project files.

---
+++ MongoDBUI/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace MongoDBUI
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Done processing MongoDB");
+
+        Console.ReadLine();
+    }
+}
