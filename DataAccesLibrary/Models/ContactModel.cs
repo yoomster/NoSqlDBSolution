4def765c1574dc536c299c5558f60125180bcb79
yoomster <nperenboom@hotmail.com>
2023-12-04 22:48:22 +0100

    Added Models folder and the needed contact models.

---
+++ DataAccesLibrary/Models/ContactModel.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLibrary.Models
+{
+    public class ContactModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public List<EmailAddressModel> EmailAddresses { get; set; } = new List<EmailAddressModel>();
+
+        public List<PhoneNumberModel> phoneNumbers { get; set; } = new List<PhoneNumberModel>();
+    }
+}
