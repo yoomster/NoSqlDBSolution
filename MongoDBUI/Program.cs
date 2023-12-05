cfd54ba203048eb875ebdc95ac50e9b2b2fda25d
yoomster <nperenboom@hotmail.com>
2023-12-05 13:27:32 +0100

    Added method for removing phonenumber from user

--- MongoDBUI/Program.cs
+++ MongoDBUI/Program.cs
@@ -35,13 +35,27 @@ namespace MongoDBUI
             // guid np: b8a2ac7d-0340-4d4d-805a-f6621fda7d16
             //guid ak: fa7e0b65-2c0e-480d-8ab9-a315b8bb462d
 
-            UpdateFirstName("Nayoomi", "b8a2ac7d-0340-4d4d-805a-f6621fda7d16");
-            GetallContacts();
+            //UpdateFirstName("Nayoomi", "b8a2ac7d-0340-4d4d-805a-f6621fda7d16");
+            //GetallContacts();
 
+
+            RemovePhoneNumberFromUser("0612345678", "b8a2ac7d-0340-4d4d-805a-f6621fda7d16");
+            
 
             Console.WriteLine("Done processing MongoDB");
             Console.ReadLine();
         }
+        private static void RemovePhoneNumberFromUser(string phoneNumber, string id)
+        {
+            Guid guid = new Guid(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+
+            contact.PhoneNumbers = contact.PhoneNumbers.Where(x => x.PhoneNumber != phoneNumber).ToList();
+
+            db.UpsertRecord(tableName, contact.Id, contact);
+
+        }
+
         private static void UpdateFirstName(string firstName, string id)
         {
             Guid guid = new Guid(id);
@@ -51,6 +65,7 @@ namespace MongoDBUI
 
             db.UpsertRecord(tableName, contact.Id, contact);
         }
+
         private static void GetContactById(string id)
         {
             Guid guid = new Guid(id);
@@ -68,6 +83,7 @@ namespace MongoDBUI
                 Console.WriteLine($"{contact.Id}: {contact.FirstName} {contact.LastName}");
             }
         }
+
         private static void CreateContact(ContactModel contact)
         {
             db.UpsertRecord(tableName, contact.Id, contact);
