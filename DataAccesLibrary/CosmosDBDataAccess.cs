61bc7f957575f680fd7604c30d4789a0b45ea570
yoomster <nperenboom@hotmail.com>
2023-12-11 21:01:48 +0100

    Added code for connecting to Cosmos

---
+++ DataAccesLibrary/CosmosDBDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLibrary
+{
+    public class CosmosDBDataAccess
+    {
+        private readonly string _endpointUrl;
+        private readonly string _primaryKey;
+        private readonly string _databaseName;
+        private readonly string _containerName;
+        private CosmosClient _cosmosClient;
+        private Database _database;
+        private Container _container;
+
+        public CosmosDBDataAccess(string endpointUrl,
+                                  string primaryKey,
+                                  string databaseName,
+                                  string containerName)
+        {
+            _endpointUrl = endpointUrl;
+            _primaryKey = primaryKey;
+            _databaseName = databaseName;
+            _containerName = containerName;
+
+            _cosmosClient = new CosmosClient(_endpointUrl, _primaryKey);
+            _database = _cosmosClient.GetDatabase(_databaseName);
+            _container = _database.GetContainer(_containerName);
+        }
+    }
+}
