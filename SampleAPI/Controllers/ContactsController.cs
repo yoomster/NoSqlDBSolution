c376eb7a6ec7ee2ba33f560d1c4fe1e56096f6ab
yoomster <nperenboom@hotmail.com>
2024-01-10 15:47:19 +0100

    Added API project

---
+++ SampleAPI/Controllers/ContactsController.cs
@@ -0,0 +1,20 @@
+﻿using DataAccesLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        private MongoDBDataAccess db;
+        private readonly IConfiguration _config;
+        private readonly string tableName = "Contacts";
+
+        public ContactsController(IConfiguration config)
+        {
+            _config = config;
+            db = new MongoDBDataAccess("MongoContactsDB", _config.GetConnectionString("Default"));
+        }
+    }
+}
