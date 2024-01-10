6a9bfae3fd9c2db2c9b5cf4ffaae6f554d8309fc
yoomster <nperenboom@hotmail.com>
2024-01-10 17:10:52 +0100

    Added read method

--- ApiDBUI/Pages/Index.cshtml.cs
+++ ApiDBUI/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+using ApiDBUI.Models;
 
 namespace ApiDBUI.Pages
 {
@@ -14,15 +16,32 @@ namespace ApiDBUI.Pages
             _httpClientFactory = httpClientFactory;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            await GetAllContacts();
         }
 
         private async Task GetAllContacts()
         {
             var _client = _httpClientFactory.CreateClient();
-            var response = await _client.GetAsync("https://localhost:44387");
+            var response = await _client.GetAsync("https://localhost:44387/api/contacts");
+
+            List<ContactModel> contacts;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                string responseText = await response.Content.ReadAsStringAsync();
+                contacts = JsonSerializer.Deserialize<List<ContactModel>>(responseText, options);
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
         }
     }
 }
